6a8878d3ffb463c756664f429d2123f58fd1ff6f
Visal Naqvi <visalnaqvi@gmail.com>
2021-11-25 13:05:08 +0530

    first commit

--- pages/tour_packages/agra_tour_package.js
+++ pages/tour_packages/agra_tour_package.js
@@ -34,9 +34,9 @@ const Agra = () => {
             },
         ]
       };
-    const pack1 = {price:"1200",title:"3 Day Package Tour One" , f1:"3 days , 2 nights stay in Agra" ,f2:"We will book the best hotel with best services and location" , f3:"The most comfortable and econimical options of travel will be available" , f4:"Breakfast , lunch , dinner", link:"/contactus"};
-    const pack2 = {price:"2200",title:"5 Day Package Tour Two" , f1:"5 days , 4 nights stay in Agra" ,f2:"We will book the best hotel with best services and location" , f3:"The most comfortable and econimical options of travel will be available" , f4:"Breakfast , lunch , dinner", link:"/contactus"};
-    const pack3 = {price:"3200",title:"7 Day Package Tour Three" , f1:"7 days , 6 nights stay in Agra" ,f2:"We will book the best hotel with best services and location" , f3:"The most comfortable and econimical options of travel will be available" , f4:"Breakfast , lunch , dinner", link:"/contactus"};
+    const pack1 = {price:"1200",title:"3 Day Agra Package Tour" , f1:"3 days , 2 nights stay in Agra" ,f2:"We will book the best hotel with best services and location" , f3:"The most comfortable and econimical options of travel will be available" , f4:"Breakfast , lunch , dinner", link:"/contactus"};
+    const pack2 = {price:"2200",title:"5 Day Agra Package Tour" , f1:"5 days , 4 nights stay in Agra" ,f2:"We will book the best hotel with best services and location" , f3:"The most comfortable and econimical options of travel will be available" , f4:"Breakfast , lunch , dinner", link:"/contactus"};
+    const pack3 = {price:"3200",title:"7 Day Agra Package Tour" , f1:"7 days , 6 nights stay in Agra" ,f2:"We will book the best hotel with best services and location" , f3:"The most comfortable and econimical options of travel will be available" , f4:"Breakfast , lunch , dinner", link:"/contactus"};
     const pack4 = {price:"4200",title:"10 Day Agra Package Tour" , f1:"10 days , 9 nights stay in Agra" ,f2:"We will book the best hotel with best services and location" , f3:"The most comfortable and econimical options of travel will be available" , f4:"Breakfast , lunch , dinner", link:"/contactus"};
     return ( 
        
@@ -44,6 +44,8 @@ const Agra = () => {
         <Head>
       <title>Agra Tour / Trip Package - Al Azeem Tour and Travels</title>
       <link rel="icon" href="%PUBLIC_URL%/logo192.png" />
+      <html lang="en"/>
+      <meta charset="UTF-8" />
         <meta
       name="description"
       content="We provide the most economical and luxurious Agra tour package with travelling , hotel booking and meals (breakfast, lunch, dinner). Our Agra tour package is fully customizable."
@@ -61,7 +63,7 @@ const Agra = () => {
             <div className={style.shade}></div>
         </div>
 
-        <Article heading="Exlopre Agra Tour Packages" text={["Where better to go for a romantic holiday than to the great testament of love, the ",<b key="1">Taj Mahal</b> ,"? Built by the grieving Mughal Emperor Shah Jahan in memory of his late wife Mumtaz Mahal, the ",<b key="3">Taj Mahal</b>,"'s unrivaled beauty explains why it’s regarded as one of the eight wonders of the world. A visit to India wouldn’t be complete without visit the city of ", <b key="6">agra</b>," Other must-experience destinations in " , <b key="7">Agra</b>," are two other UNESCO World Heritage Sites, the sandstone Red Fort and Fatehpur Sikri. And for you to visit ",<b key="8">agra</b>," in the best way we have ",<b key="4">Agra tour package</b>," more affordable , economic and luxurious than anywhere else." ]}></Article>
+        <Article heading="Exlopre Agra Tour Packages" text={["Where better to go for a romantic holiday than to the great testament of love, the Taj Mahal? Built by the grieving Mughal Emperor Shah Jahan in memory of his late wife Mumtaz Mahal, the Taj Mahal's unrivaled beauty explains why it’s regarded as one of the eight wonders of the world. A visit to India wouldn’t be complete without visit the city of ", <b key="6">agra</b>," Other must-experience destinations in " , <b key="7">Agra</b>," are two other UNESCO World Heritage Sites, the sandstone Red Fort and Fatehpur Sikri. And for you to visit ",<b key="8">agra</b>," in the best way we have ",<b key="4">Agra tour package</b>," more affordable , economic and luxurious than anywhere else." ]}></Article>
         <Slider style={{width:"90vw",marginBottom:"50px", margin:"0 auto"}} {...settings}>
             <PackageCard packh={pack1}></PackageCard>
             <PackageCard packh={pack2}></PackageCard>
