c5336285706f9b3db1d4a5757d1e8c0865fc5b18
Visal Naqvi <visalnaqvi@gmail.com>
2022-01-16 16:24:17 +0530

    first commit

--- pages/flights.js
+++ pages/flights.js
@@ -5,6 +5,10 @@ import Article from "../comps/Article"
 import Img1 from "../public/images/aboutus/aboutimg2.png";
 import InfoCardStretch from "../comps/InfoCardStretch/InfoCardStretch";
 import styles from "../styles/Hero.module.css";
+import SliderSingle from "../comps/Packages/SliderSingle";
+import firebase from 'firebase/app';
+import { useEffect, useState } from "react";
+
 const AboutUs = () => {
     const infoText = "We believe in open and honest communications - and no hidden agendas. We believe that everyone will do the right thing and act with integrity and fairness We respect each others time and effort We treat others the way we want to be treated We are always here to help each other experience the best in travel"
     const InfoHeading = "In order to promote and reward excellence in travel, our community is guided by the following core values:"
@@ -14,7 +18,35 @@ const AboutUs = () => {
     const firstParaText = "We are very excited to introduce AL - AZEEM TOUR & TRAVELS. as the one stop provider for all your travel needs and requirements. We label ourselves as leading, innovative, and highly efficient and look forward to demonstrate these qualities at the earliest possible chance. Since established our primary aim was to provide quality travel services to business and leisure travelers. The agency continued to grow and branch out its interests to rise as one of the exponents in the travel industry. At Al Azeem, we excel at helping you get your vacation planned. Not just any vacation, but exceptional vacations filled with inspiring and life-enriching experiences.";
     const ourvaluesheading = "Our values and community";
     const ourvaluestext = "Our community is one in which discerning travelers seek the best in quality, authenticity and value, and travel companies strive to deliver the same. The best travel specialists receive the recognition they deserve and consequently get more business. Travelers and travel specialists come to Al Azeem to help each other experience the very best in travel - from both their perspectives.";
+    // const [pckages, setPack] = useState();
+    // const config={
+    //   apiKey: "AIzaSyBwGQoCe0wTlR61fueDKA0yA4n5xmMfPrg",
+    //   authDomain: "buttons-2dc4a.firebaseapp.com",
+    //   databaseURL: "https://buttons-2dc4a-default-rtdb.firebaseio.com",
+    //   projectId: "buttons-2dc4a",
+    //   storageBucket: "buttons-2dc4a.appspot.com",
+    //   messagingSenderId: "241629842019",
+    //   appId: "1:241629842019:web:6c248c713e6a27cd494656",
+    //   measurementId: "G-QK6TC8JTHZ"
+    // }
     
+    // if(!firebase.apps.length){
+    //   firebase.initializeApp(config)
+      
+    // }else{ firebase.app() };
+    // const db = firebase.firestore()
+    // useEffect(()=>{
+    
+    // db.collection("packages")
+    // .onSnapshot((querySnapshot) => {
+    //       const inpack = []
+    //       querySnapshot.forEach((doc) => {
+    //           inpack.push(doc.data());
+    //           });
+    //           setPack(inpack)
+    // })
+    
+    // }, [])
     return ( <>
       <Head>
       <title>Al Azeem Tour & Travels - Affordable Flight Tickets</title>
@@ -27,6 +59,8 @@ const AboutUs = () => {
         <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
 
       </Head>
+      <div className={`${style.wrapperd} dflex-class`}>
+      <div className={style.leftd}>
       <div className={`${styles.slide} ${styles.flight} bg-class flex-class`}>
             
             <div className={styles.content}>
@@ -35,12 +69,56 @@ const AboutUs = () => {
             </div>
             <div className={styles.shade}></div>
         </div>
+        
+
+
         <br></br>
 <br></br>
 <br></br>
 <br></br>
 <br></br>
         <div className={styles.body}>
+        <Headings h1="International  tickets from Lucknow"></Headings>
+        <div className={style.tablewrap}>
+        <table className={`${style.p} ${style.table}`}>
+            <tr className={style.tr}>
+              <th className={style.th}>Airlines</th>
+              <th className={style.th}>Sector</th>
+              <th className={style.th}>Dates</th>
+              <th className={style.th}>Dep. Time</th>
+              <th className={style.th}>Arrival Time</th>
+              <th className={style.th}>Fair</th>
+            </tr>
+            
+            <tr className={style.tr}>
+            <td className={style.td}>VISTARA</td>
+            <td className={style.td}>DEL DXB</td>
+            <td className={style.td}>19/20 JAN</td>
+            <td className={style.td}>21.45</td>
+            <td className={style.td}>00.30</td>
+            <td className={style.td}>Rs.15,500/-</td>
+
+            </tr>
+
+            <tr className={style.tr}>
+            <td className={style.td}>AIR INDIA EXPRESS </td>
+            <td className={style.td}>VNS SHJ</td>
+            <td className={style.td}>21/27 JAN</td>
+            <td className={style.td}>16.15</td>
+            <td className={style.td}>19.00</td>
+            <td className={style.td}>Rs.20,000/-</td>
+
+            </tr>
+
+    
+           
+        </table>
+        </div>
+        <br></br>
+<br></br>
+<br></br>
+<br></br>
+<br></br>
         <Headings h1="New Delhi to Riyadh"></Headings>
         <div className={style.tablewrap}>
         <table className={`${style.p} ${style.table}`}>
@@ -291,12 +369,19 @@ const AboutUs = () => {
 
 </div>
         </div>
+        </div>
+        {/* <div className={style.right}>
+          <div className={style.heroWrapper}>
+          {pckages && <SliderSingle packages = {pckages}></SliderSingle>}  
+          </div>
+        </div> */}
 
         <br></br>
 <br></br>
 <br></br>
 <br></br>
 <br></br>
+</div>
 
     </> );
 }
