1718bd0fb2cac4c109c9cdca966833c62d73145a
unknown <visalnaqvi@gmail.com>
2022-08-31 20:13:15 +0530

    mobile version correct

--- pages/index.js
+++ pages/index.js
@@ -70,11 +70,11 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-      <title>Al Azeem Tour & Travels - Saudi Arabia Tour Packages</title>
+      <title>Al Azeem Tour 	&#x00026; Travels - INT./DOM. Flight Ticketing, Visa Assistance, Holiday Packages</title>
       <link rel="icon" href="%PUBLIC_URL%/logo192.png" />
         <meta
       name="description"
-      content="Best Tour and Travels Agency in Delhi. Saudi Arabia Tour Package and Hajj Umrah Tour Package available. Hajj Umrah economic tour packages. We have trips available all over India. Features including Hotel Booking , Travel Planning etc"
+      content="Best Tour and Travels Agency in Delhi. INnternational and Domestic Flight Ticketing, Visa Assistance, Holiday Packages and Hajj Umrah Tour Package available. Hajj Umrah economic tour packages. We have trips available all over India. Features including Hotel Booking , Travel Planning etc"
     />
         <link rel="icon" href="/logo512.png" />
         <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
