8dfe18062485aa3c2442ece930416beda10d38a1
visal.naqvi <visal.naqvi@electrifai.com>
2023-04-01 15:34:20 +0530

    new haj page

--- pages/umrahPackage.js
+++ pages/umrahPackage.js
@@ -143,7 +143,7 @@ Haj packages 2023,
           </div>
         
         </div>
-        <div className={styl.hajjPkgBox}>
+        {/* <div className={styl.hajjPkgBox}>
           <div className={styl.HajjBoxLeft}>
             <h3>HAJ 2023 - Tentative Haj 2023 Pkg</h3>
             <p className={styl.tag}>SHIFTING PACKAGE <strong>35 – 40 Days</strong></p>
@@ -163,7 +163,7 @@ Haj packages 2023,
           </div>
           </div>
         
-        </div>
+        </div> */}
       </div>
 
 
