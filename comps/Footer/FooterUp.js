fcbf87f76d52f67f508fcb40a9056ccd293b0459
visal.naqvi <visal.naqvi@electrifai.com>
2023-06-24 20:23:16 +0530

    new fixes

--- comps/Footer/FooterUp.js
+++ comps/Footer/FooterUp.js
@@ -17,7 +17,7 @@ const FooterUp = () => {
         <div className={fs.left}>
             <p className={fs.title}>Contact Us for more information</p>
             <p className={fs.text}>Planning for Umrah? Book your umrah package with us as we provide the highest-quality Umrah packages from India at amazingly reasonable prices, ensuring that all of your travel needs are met to your satisfaction.</p>
-            <p className={fs.text} style={{marginTop:"10px",letterSpacing:"1px",fontSize:"1.2rem"}}><strong>Email:  aatravels1170@gmail.com</strong></p>
+            <p className={`${fs.text} ${fs.ftbold}`} style={{marginTop:"10px",letterSpacing:"1px",fontSize:"1.2rem"}}><strong>Email:  aatravels1170@gmail.com</strong></p>
         </div>
         <div className={`${fs.right} flex-class`}>
         <div>
